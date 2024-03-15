29f76a3d92775c0347fd1f7a3448e977f2ff607c
amarbkc <amarkc9702@gmail.com>
2024-03-14 21:15:58 -0400

    Final　Commit

--- large_scale_programming_amar_kc/src/org/howard/edu/lsp/midterm/question2/Driver2.java
+++ large_scale_programming_amar_kc/src/org/howard/edu/lsp/midterm/question2/Driver2.java
@@ -1,10 +1,6 @@
 package org.howard.edu.lsp.midterm.question2;
-
 /**
- * Occult Range Tester: An esoteric script designed to explore the capabilities of the
- * enigmatic IntegerRange class.
- * This script delves into the abyss of uncertainty, seeking answers to questions
- * that transcend the boundaries of human comprehension.
+ * Tester file to test the IntegerSet class which implements Range Interface
  */
 public class Driver2 {
     public static void main(String[] args) {
@@ -13,9 +9,9 @@ public class Driver2 {
         Range arcaneRange2 = new IntegerRange(15, 25);
 
         // Casting spells to unveil the secrets of the enigmatic ranges
-        System.out.println("Is the number 15 shrouded within the first arcane range? " + arcaneRange1.contains(15));
-        System.out.println("Is there a mysterious overlap between the two arcane ranges? " + arcaneRange1.overlaps(arcaneRange2));
-        System.out.println("The size of the first arcane range remains a mystery: " + arcaneRange1.size());
-        System.out.println("The first and second arcane ranges share a common secret: " + arcaneRange1.equals(arcaneRange2));
+        System.out.println("Is the number 15 within the first arcane range? " + arcaneRange1.contains(15));
+        System.out.println("Is there a overlap between the two arcane ranges? " + arcaneRange1.overlaps(arcaneRange2));
+        System.out.println("The size of the first arcane range is " + arcaneRange1.size());
+        System.out.println("The first and second arcane ranges share a common intervals " + arcaneRange1.equals(arcaneRange2));
     }
 }
